94d90a6bec9dfda3dd5fd1957a210bc854d5f2b7
Bohdan Lytvyniuk <lytvyniukbohdan@gmail.com>
2023-09-19 13:46:35 +0200

    clear App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,8 @@
-// import { ContactsForm } from './ContactsForm/ContactsForm';
-// import { Section } from './Section/Section';
-// import { ContactList } from './ContactsList/ContactList';
-// import Filter from './Filter/Filter';
-// import { useDispatch } from 'react-redux';
-// import { addContact } from 'redux/Contacts/contactsOperation';
+
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../Pages/Home';
 import { Contacts } from '../Pages/contacts';
 import { Layout } from './Layout';
-import { LoginForm } from './LoginForm/LoginForm';
-import { RegisterForm } from './RegisteForm/RegisterForm';
-import { useSelector } from 'react-redux';
-import { selectIsLoggedIn } from 'redux/auth/selectors';
 import Register from 'Pages/register';
 import Login from 'Pages/Login';
 
@@ -21,7 +12,6 @@ export function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/register" element={<Register/>}/>
           <Route path="/login" element ={<Login/>}/>
